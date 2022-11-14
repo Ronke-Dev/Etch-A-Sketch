04ee6968bd1b33acac0400a8232493e4ec114e82
Ronke <deronke95@gmail.com>
2022-11-14 23:27:45 +0100

    rearranged the files

--- script.js
+++ script.js
@@ -1,241 +1,128 @@
-// create a dom manipulation
+// create a random number generator
+// create a random color generator that generates 256 numbers
+// assign a new element to the existing container and add a class
+// create a button function using the mouse over event Listener
+// create a for loop that expands the random number generator
+// create an input Box
 
-// Created a variable that held the grid container.
-const container = document.querySelector('#grid_container');
 
 
-// Create a function that generates random numbers
-function generateNumber(number){
-  return  Math.floor(Math.random() * number + 1)
-}
 
-// Alternative to this
-// function generateNumber(){
-//     return  Math.floor(Math.random() * 256)
-//   }
-  
+// create a random number generator function
+// using 255 because numbers will be generated from 0-255 = 256 numbers, 16 X 16 = 256
 
-// create a function that generates rows and column and append the new divs to it
-// assign style to container
-// we are assigning an eventlistener, in this case that we want the colour of the grids to change
-// when we hover, that move mouse over it., the we will create a function and assign it to the event listener
-// passing what we want to happen when the mouse overs
+function randomNumber(){
+    return Math.floor(Math.random() * 255 + 1)
+};
 
-function createRows(rows,column){
-    container.style.setProperty("--grid-rows", rows);
-    container.style.setProperty("--grid-cols", cols);
+randomNumber();
 
-    for(let i = 0; i < rows * column; i++){
-        let cells = document.createElement("div");
-        cells.classList.add("gridItem");
-        container.appendChild(cells);
-        cells.addEventListener("mouseover", changeColor())
+
+
+    
+
+// A function that will change the background colour randomly, rgb uses 3 values
+
+
+
+// assign a new element to the existing grid container and add a class
+const gridContainer = document.querySelector('#grid_container');
+
+
+// create a function that produces multiple grid, by using a for loop.then insert the new element
+function makeRows(rows, cols){
+    gridContainer.style.setProperty("--grid-rows",rows);
+    gridContainer.style.setProperty("--grid-cols", cols);
+    for(c = 0; c < (rows * cols); c++){
+    const cell = document.createElement("div");
+    cell.classList.add("grid-item");
+    gridContainer.appendChild(cell);
+    cell.addEventListener("mouseover", function changeColor(){
+      const rndCol = `rgb(${randomNumber(255)},${randomNumber(255)},${randomNumber(255)})`;
+      cell.style.backgroundColor = rndCol;
+    });
+   
+    
 
     }
-
 }
 
-// create a function that will change the colors randomly, then pass  ot as a paraameter to the eventlistener above
-function changeColor(){
-    const randomColor = `(${generateNumber(255)}, ${generateNumber(255)}, ${generateNumber(255)})`;
-    cells.style.backgroundColor = randomColor;
-}
-
-createRows(16,16);
+makeRows(16,16)
 
 
 
 
-const button = document.getElementById("button");
+// make the button
+const button = document.querySelector("#btn");
 
-button.addEventListener("click", function changeSize(size) {
-    size = prompt("Enter a value between 0 to 101");
-    container.innerHTML = "";
-    if (0 < size && size <= 100) {
-        makeRows(size, size);
-    } else {
-        makeRows(16, 16);   
-        alert ("Only type values between 1 to 100");
-    }
+button.addEventListener("click", function changeSize(size){
+  size = prompt("Enter a value between 0 and 101");
+  gridContainer.innerHTML="";
+  if (0 < size && size <= 100){
+    makeRows(size,size);
+  } else{ 
+    makeRows(16,16);
+    alert("only input numbers between 0 and 101")}
 });
 
 
+  
+ 
 
 
 
 
 
+// button.addEventListener("click", function changeSize(size) {
+//   size = prompt("Enter a value between 0 to 101");
+//   container.innerHTML = "";
+//   if (0 < size && size <= 100) {
+//       makeRows(size, size);
+//   } else {
+//       makeRows(16, 16);   
+//       alert ("Only type values between 1 to 100");
+//   }
+// });
 
 
 
 
 
-
-
-
-
-
-
-
-
-// selector {
-// 	background-color: black;
+// // generate random colour through the using of template literals
+// function changeColor(){
+//     const rndCol = `rgb(${randomNumber(255)},${randomNumber(255)},${randomNumber(255)})`;
+//     document.body.style.//////////////////////// = rndCol; - we will have to input the grid background here and not the assignments main background, assign it to div, check TOP
 // }
 
-// .selector:hover {
-// 	background-color: blue;
 
-
-
-
-
-
-
-
-
-
-// const grid_1 = document.createElement('div');
-// grid_1.classList.add('grid_item');
-// grid_1.textContent = "";
-
-// container.appendChild(grid_1);
-
-// const row 
-
-// function createRows(number){
-//     let i = 1; i <= number*number; i++;
-//     return number 
     
-// }
 
 
+    // const grid_container = document.querySelector('#grid_container');
+    // const cells = document.createElement("div");
+    // cells.classList.add("cells");
+    // cells.textContent= "TEXT";
+    // grid_container.appendChild(cells);
 
 
-
-
-
-// function createGrid(number) {
-//     const gridContainer = document.querySelector(".grid-container");
-//     gridContainer.style.gridTemplateColumns = `repeat(${number}, 1fr)`;
-//     gridContainer.style.gridTemplateRows = `repeat(${number}, 1fr)`;
-//     for (let i = 1; i <= number*number; i++) {
-//         let div = document.createElement("div");
-//         div.classList.add("grid-box")
-//         gridContainer.appendChild(div);
-//     }
-//     document.querySelector(".grid-dimensions").textContent = `${number}x${number}`;
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function createRows(rows,number){
-//     let text = 0
-//     let i = 0;
-//     while (number > 0){
-//         text += rows;
-//         number--;
-//     }
-//     return text * 16
-// }
-
-// createRows(grid_1,16)
-
-// function createRows(rows,number){
-//     let text = 0
-//     let i = 0;
-//     while (number > 0){
-//         text += rows;
-//         number--;
-//     }
-//     return text * 16
-// }
-
-// createRows(grid_1,16)
-
-// function createRows(){
     
-//     let i = 0;
-//     while (i < 10){
-//         grid_1 += i;
-//         i++;
-//     }
-//     return grid_1
-// }
-
-// createRows()
 
 
-// {/* <script>
-// let text = "";
-// let i = 0;
-// while (i < 10) {
-//   text += "<br>The number is " + i;
-//   i++;
-// } */}
+    // function changeColor(){
+    //     const rndCol = `rgb(${randomNumber(255)},${randomNumber(255)},${randomNumber(255)})`;
+    //     document.body.style.cells = rndCol;
+       
+    // }
 
 
+  
 
-
-
-
-
-
-
-
-
-
-// const grid_3 = document.createElement('div');
-// grid_3.classList.add('grid_item');
-// grid_3.textContent = "1";
-
-// container.appendChild(grid_3);
-
-
-// const grid_2 = document.createElement('div');
-// grid_2.classList.add('grid_item');
-// grid_2.textContent = "4";
-
-// container.appendChild(grid_2);
-
-
-// const grid_4 = document.createElement('div');
-// grid_4.classList.add('grid_item');
-// grid_4.textContent = "5";
-
-// container.appendChild(grid_4);
-
-
-// const repeatString = function (str,num){
-//     let text = "";
-//     if(num < 0){return "ERROR"}
-//     while(num > 0){
-//         text += str;
-//         num--;
-//     }
-
-//     return text
-
-
-// }
     
 
 
 
 
+// Use Javascript to add cells to the grid container
 
 
 
@@ -243,25 +130,116 @@ button.addEventListener("click", function changeSize(size) {
 
 
 
-// the for loop i will use 
 
-// <!DOCTYPE html>
-// <html>
-// <body>
+//create a button that when you click it and input button shows up
 
-// <h2>JavaScript While Loop</h2>
+// function message(){
+//     let produce = prompt("Enter a value between 0 to 100");
+//     return produce
+// };
 
-// <p id="demo"></p>
 
-// <script>
-// let text = "";
-// let i = 0;
-// while (i < 10) {
-//   text += "<br>The number is " + i;
-//   i++;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// create a function for the Message
+// function message(){
+//     change =  prompt("Enter a value between 0 to 100")
+//     return change
+// };
+
+// make the button
+// const button = document.getElementById("btn");
+// button.addEventListener("click", message());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// assign a new element to the existing container and add a class
+// const container = document.querySelector("#container");
+
+// const newDiv = document.createElement('div');
+// newDiv.classList.add("grid");
+// newDiv.textContent = "Hi, I want to be a grid";
+
+// container.appendChild(newDiv);
+
+
+
+
+
+
+
+
+
+
+
+// newDiv.style.backgroundColor = randomColor; find a way to make sure the random colour is the background of the grid
+
+
+
+// / create a random color generator that generates 256 numbers
+// The value of the Hexadecimal can range from 00 to FF. So, the maximum value can be FFFFFF - white color, and the minimum value can be 000000 - black color
+// `assign maximum color value to a variable
+// assign the random color number generator to a variable
+// then convert the random coloR number to A HEX AGAIN (string(16)
+// then use padStart to ensure that the string length is at least 6
+
+// a shorter way to write it
+
+// const randColor = () =>  {
+//     return "#" + Math.floor(Math.random()*16777215).toString(16).padStart(6, '0').toUpperCase();
 // }
-// document.getElementById("demo").innerHTML = text;
-// </script>
 
-// </body>
-// </html>
+// console.log(randColor());
+
+// function generateRandomColor(){
+//     let maximumValue = 0xFFFFFF;
+//     let randomColorNumber = Math.floor(Math.random() * maximumValue);
+//     randomColorNumber = randomColorNumber.toString(16);
+//     let randomColor = randomColorNumber.padStart(6,0);
+//     return `#${randomColor.toUpperCase()}`
+// };
+
+// generateRandomColor();
+
+
+// random color generator multiplied by 255 random numbers
+// what if i input the randomNumber() 256 function i made at the top here? This means it wont generate more that 256 numbers, it worked
+
+// function generateRandomColor(){
+//     let maximumValue = 0xFFFFFF;
+//     let randomColorNumber = randomNumber() * maximumValue;
+//     randomColorNumber = randomColorNumber.toString(16);
+//     let randomColor = randomColorNumber.padStart(6,0);
+    
+//     return `#${randomColor.toUpperCase()}`
+// };
+
+// generateRandomColor();
